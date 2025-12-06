40a5f28a5db99c16e9e2fbe5652b3c1534038990
j3jangid <jatinjangid97@gmail.com>
2025-12-06 14:34:57 +0530

    mongo atlas added

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// import express from 'express'
-// import cors from 'cors'
-// import dotenv from 'dotenv'
-// import { connectDB } from './src/config/connectDB.js'
-
-// dotenv.config()
-
-// const PORT = process.env.PORT || 3534
-
-// const app = express()
-
-// app.use(cors())
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-
-// app.get('/', (req, res) => {
-//     res.send('hii from BE')
-// })
-
-
-
-
-// connectDB().then(() => {
-//     app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
-// }).catch((err) => console.log("Error While Connecting DB:", err))
-
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
